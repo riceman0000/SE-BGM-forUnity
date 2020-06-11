ce6cae7af9c4685ccc7a5e8b58c49b0004773e1b
riceman0000 <45267493+riceman0000@users.noreply.github.com>
2020-06-11 17:05:27 +0900

    1st commit

---
+++ AudioPlayer.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+namespace any
+{
+
+
+    /// <summary>
+    /// オーディオを再生させる。
+    /// </summary>
+    public class AudioPlayer : ManagedMono
+    {
+        /// <summary>
+        /// オーディオソース。
+        /// </summary>
+        private AudioSource loop;
+        private AudioSource intro;
+        public AudioSource Loop
+        {
+            get
+            {
+                if (loop == null)
+                {
+                    loop = AudioManager.Instance.Bgm;
+                }
+                return loop;
+            }
+        }
+        public AudioSource Intro
+        {
+            get
+            {
+                if (intro == null)
+                {
+                    intro = AudioManager.Instance.Intro;
+                }
+                return intro;
+            }
+        }
+
+        [SerializeField] AudioList audioList;
+
+        AudioManager _audioManager;
+        /// <summary>
+        /// シーンごとにAudioSourceを変更させる
+        /// </summary>
+        public void SourceChanged(SceneTitle title)
+        {
+            var currentTitle = title == SceneTitle.None ? SceneTitle.StageOne : title;
+            var audioList = this.audioList.AudioDics[currentTitle];
+            Loop.clip = audioList.loopClip;
+            Loop.loop = true;
+            if (audioList.introFlag)
+            {
+                Intro.clip = audioList.introClip;
+                Intro.loop = false;
+                Intro.Play();
+                Loop.PlayScheduled(AudioSettings.dspTime + Intro.clip.length);
+            }
+            else { Loop.Play(); }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _audioManager = AudioManager.Instance;
+            _audioManager.RegistPlayer(this);
+
+            SceneFader.OnSceneLoaded += SourceChanged;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void OnDisable()
+        {
+            _audioManager?.UnregistPlayer(this);
+        }
+
+        /// <summary>
+        /// 再生を行う。
+        /// </summary>
+        public void Play()
+        {
+            // 自分がBgmの場合には、他のBgmの再生を停止させます。
+            var bgm = AudioManager.Instance.AudioBgmPlayers;
+            if (bgm.Contains(this))
+            {
+                foreach (var player in bgm)
+                {
+                    if (player == this) { continue; }
+
+                    if (player.Loop.isPlaying)
+                    {
+                        {
+                            player.Stop();
+                        }
+                    }
+                }
+            }
+
+            Loop.volume = 0;
+            Loop.Play();
+        }
+
+        /// <summary>
+        /// 停止を行う。
+        /// </summary>
+        public void Stop()
+        {
+            Loop.Stop();
+        }
+
+        /// <summary>
+        /// 一時停止する
+        /// </summary>
+        public void Pause()
+        {
+            Loop.Pause();
+        }
+    }
+}
---
+++ SEList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable, CreateAssetMenu(fileName = "SEList", menuName = "SEList/Create")]
+
+public class SEList : ScriptableObject
+{
+    public List<SEContent> sEContents = new List<SEContent>();
+    Dictionary<SETag, AudioClip> seDics = new Dictionary<SETag, AudioClip>();
+
+    public Dictionary<SETag, AudioClip> SeDics
+    {
+        get
+        {
+            if (seDics == null || seDics.Count == 0)
+            {
+                foreach (var item in sEContents)
+                {
+                    seDics.Add(item.SEName, item.SEClip);
+                }
+            }
+            return seDics;
+        }
+
+    }
+}
+[System.Serializable]
+public class SEContent
+{
+    public SETag SEName;
+    public AudioClip SEClip;
+}
+
+public enum SETag
+{
+    /*
+    //any tag
+    /// <summary>選択音</summary>
+    Select,
+    /// <summary>敵の発砲音</summary>
+    Shoot,
+    /// <summary>リザルト画面で鳴る音</summary>
+    Result
+    */
+};
